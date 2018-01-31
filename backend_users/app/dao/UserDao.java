6f0009fa0fc1cd4ff423dabcd619f0b94bd9af5a
Manasa Jonnalagadda <jonnalagaddamanasa7@gmail.com>
2018-01-31 13:53:16 +0530

    TODO OAuth2

--- backend_users/app/dao/UserDao.java
+++ backend_users/app/dao/UserDao.java
@@ -1,58 +1,65 @@
-package dao;
+    package dao;
 
 
-import com.google.inject.Inject;
-import models.User;
-import org.apache.commons.lang3.RandomStringUtils;
-import play.db.jpa.JPAApi;
+    import com.google.inject.Inject;
+    import models.User;
+    import org.apache.commons.lang3.RandomStringUtils;
+    import play.db.jpa.JPAApi;
 
-import javax.persistence.Query;
-import javax.persistence.TypedQuery;
-import java.util.List;
+    import javax.persistence.Query;
+    import javax.persistence.TypedQuery;
+    import java.util.List;
+    import java.util.Random;
 
-public class UserDao implements SignUpDao<User> {
-
-
-    private JPAApi jpaApi;
-
-    @Inject
-    public UserDao(JPAApi jpaApi) { this.jpaApi = jpaApi; }
+    public class UserDao implements SignUpDao<User> {
 
 
-    public User persist(User user) {
+        private JPAApi jpaApi;
 
-        jpaApi.em().persist(user);
+        @Inject
+        public UserDao(JPAApi jpaApi) { this.jpaApi = jpaApi; }
 
-        return user;
+
+        public User persist(User user) {
+
+            jpaApi.em().persist(user);
+
+            return user;
+        }
+
+        public List<User> findAll() {
+
+            TypedQuery<User> query = jpaApi.em().createQuery("SELECT u FROM User u", User.class);
+            List<User> users = query.getResultList();
+
+            return users;
+        }
+
+        public List<User> getUser(String userName) {
+
+            String str = "SELECT u"+ " FROM User u WHERE u.userName = :name";
+            TypedQuery<User> query = jpaApi.em().createQuery(str, User.class);
+            query.setParameter("name", userName);
+
+            List<User> result = query.getResultList();
+
+            return result;
+        }
+
+        public String generateToken(String userName) {
+
+            //String token= RandomStringUtils.randomAlphanumeric(22);
+            Random r = new Random(); // perhaps make it a class variable so you don't make a new one every time
+            StringBuilder token = new StringBuilder();
+            for(int i = 0; i < 22; i++) {
+                char c = (char)(r.nextInt((int)(Character.MAX_VALUE)));
+                token.append(c);
+            }
+
+            User b = jpaApi.em().find(User.class,userName);
+            b.setToken(token.toString());
+            return token.toString();
+        }
+
+
     }
-
-    public List<User> findAll() {
-
-        TypedQuery<User> query = jpaApi.em().createQuery("SELECT u FROM User u", User.class);
-        List<User> users = query.getResultList();
-
-        return users;
-    }
-
-    public List<User> getUser(String userName) {
-
-        String str = "SELECT u"+ " FROM User u WHERE u.userName = :name";
-        TypedQuery<User> query = jpaApi.em().createQuery(str, User.class);
-        query.setParameter("name", userName);
-
-        List<User> result = query.getResultList();
-
-        return result;
-    }
-
-    public String generateToken(String userName) {
-
-        String token= RandomStringUtils.randomAlphanumeric(22);
-
-        User b = jpaApi.em().find(User.class,userName);
-        b.setToken(token);
-        return token;
-    }
-
-
-}
--- backend_users/app/models/User.java
+++ backend_users/app/models/User.java
@@ -1,84 +1,84 @@
-package models;
+    package models;
 
-import com.fasterxml.jackson.annotation.JsonIgnore;
-import javax.persistence.*;
+    import com.fasterxml.jackson.annotation.JsonIgnore;
+    import javax.persistence.*;
 
-@Entity
-public class User {
+    @Entity
+    public class User {
 
-    @Id
-    String userName;
+        @Id
+        String userName;
 
-    @Basic
-    String password;
+        @Basic
+        String password;
 
-    @Basic
-    String email;
+        @Basic
+        String email;
 
-    @Basic
-    Integer role;
+        @Basic
+        Integer role;
 
-    @Basic
-    private String token;
+        @Basic
+        private String token;
 
 
-    public User(String userName, String password) {
-        this.userName = userName;
-        this.password = password;
+        public User(String userName, String password) {
+            this.userName = userName;
+            this.password = password;
+        }
+
+        public User(String userName) {
+            this.userName = userName;
+        }
+
+
+        public User() {
+
+        }
+
+        public String getUserName() {
+            return userName;
+        }
+
+        public void setUserName(String userName) {
+            this.userName = userName;
+        }
+
+        //@JsonIgnore
+        public String getPassword() {
+            return password;
+        }
+
+        public void setPassword(String password) {
+            this.password = password;
+        }
+
+        public String getEmail() {
+            return email;
+        }
+
+        public void setEmail(String email) {
+            this.email = email;
+        }
+
+        @JsonIgnore
+        public Integer getRole() {
+            return role;
+        }
+
+        public void setRole(Integer role) {
+            this.role = role;
+        }
+
+       @JsonIgnore
+        public String getToken() {
+            return token;
+        }
+
+        public void setToken(String token) {
+            this.token = token;
+
+
+
+        }
     }
-
-    public User(String userName) {
-        this.userName = userName;
-    }
-
-
-    public User() {
-
-    }
-
-    public String getUserName() {
-        return userName;
-    }
-
-    public void setUserName(String userName) {
-        this.userName = userName;
-    }
-
-    //@JsonIgnore
-    public String getPassword() {
-        return password;
-    }
-
-    public void setPassword(String password) {
-        this.password = password;
-    }
-
-    public String getEmail() {
-        return email;
-    }
-
-    public void setEmail(String email) {
-        this.email = email;
-    }
-
-    @JsonIgnore
-    public Integer getRole() {
-        return role;
-    }
-
-    public void setRole(Integer role) {
-        this.role = role;
-    }
-
-   @JsonIgnore
-    public String getToken() {
-        return token;
-    }
-
-    public void setToken(String token) {
-        this.token = token;
-
-
-
-    }
-}
